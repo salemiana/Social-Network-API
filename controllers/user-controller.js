49b9ac9cb146058257004699ca4a81d22650d537
salem z <salem.zerezghi2@gmail.com>
2022-07-14 17:57:04 -0600

    user-controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+  getUsersById,
+  createUser,
+  updateUser,
+  deleteUser
+} = require('../../controllers/user-controller');
+
+// /api/users
+router
+  .route('/')
+  .get(getAllUsers)
+  .post(createUser);
+
+// /api/User/:id
+router
+  .route('/:id')
+  .get(getUsersById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
